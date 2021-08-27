4064152924ef66a53499c9c8e3de6ee34b1c8930
Sebastián <sebas4101p@estudiantec.cr>
2021-08-27 02:57:19 -0600

    Avance 2

--- src/Interfaz_Cliente_C.java
+++
@@ -1,2 +0,0 @@
-public class Interfaz_Cliente_C {
-}
--- src/MainFrameClient.java
+++ src/MainFrameClient.java
@@ -1,2 +1,44 @@
-public class MainFrameClient {
-}
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.Socket;
+
+public class MainFrameClient extends JFrame{
+    private JTextField Lector;
+    private JButton miboton;
+    private JPanel Principal;
+    private JLabel Texto1;
+    private JTextArea textArea1;
+
+    public  MainFrameClient(){
+        setContentPane(Principal);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setResizable(false);
+        setSize(400,400);
+        setTitle("Service");
+        Enviar_Texto mienvento=new Enviar_Texto();
+        miboton.addActionListener(mienvento);
+
+    }
+    private class Enviar_Texto implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            //System.out.println(Lector.getText());
+            try {
+                Socket misocket=new Socket("192.168.0.14",9999);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+
+        }
+    }
+    public static void main(String[] args) {
+        MainFrameClient Service = new MainFrameClient();
+
+    }
+
+    }
